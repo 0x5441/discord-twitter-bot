03358d1d7fdf6b83e341a59d9ce4cfa0ef1975f9
0x5441 <the4root104@gmail.com>
2025-11-18 23:51:59 +0800

    Refactor tweet sending to use bearer token
    Updated the tweet functionality to use Twitter API v2 for sending tweets with bearer token authentication.

--- tweet.py
+++ tweet.py
@@ -1,17 +1,41 @@
-import tweepy
+import requests
 import os
+import base64
 
-# Load keys from environment
-api_key = os.getenv("API_KEY")
-api_secret = os.getenv("API_SECRET")
-access_token = os.getenv("ACCESS_TOKEN")
-access_secret = os.getenv("ACCESS_SECRET")
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
 
-auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_secret)
-api = tweepy.API(auth)
+# Twitter token endpoint
+token_url = "https://api.twitter.com/oauth2/token"
 
-# Tweet text
-tweet = "هنا تحط التغريدة الي تبغاها تظهر كل ساعتين"
+# Encode client_id:client_secret
+key_secret = f"{client_id}:{client_secret}".encode("ascii")
+b64_encoded_key = base64.b64encode(key_secret).decode("ascii")
 
-api.update_status(tweet)
-print("Tweet sent!")
+headers = {
+    "Authorization": f"Basic {b64_encoded_key}",
+    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+}
+
+data = {"grant_type": "client_credentials"}
+
+# Get bearer token
+response = requests.post(token_url, headers=headers, data=data)
+access_token = response.json().get("access_token")
+
+# Tweet endpoint
+tweet_url = "https://api.twitter.com/2/tweets"
+
+tweet_text = "تغريدتك هنا يا سلطان كل ساعتين"
+
+tweet_payload = {"text": tweet_text}
+
+tweet_headers = {
+    "Authorization": f"Bearer {access_token}",
+    "Content-Type": "application/json",
+}
+
+tweet_response = requests.post(tweet_url, headers=tweet_headers, json=tweet_payload)
+
+print(tweet_response.status_code)
+print(tweet_response.text)
